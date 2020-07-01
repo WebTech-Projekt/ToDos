eb5087989cd6df4634934f3264adc8a957299aa9
Nestor Mermoz Thea <nestormermozthea@gmail.com>
2020-07-01 06:43:26 +0200

    business rest api based

---
+++ persistence/src/main/java/de/webprojekt/dto/NewTodo.java
@@ -0,0 +1,78 @@
+package de.webprojekt.models;
+
+import java.util.Date;
+
+public class NewTodo {
+
+    private Long id;
+    private String title;
+    private String content;
+    private boolean isDone;
+    private Date deadline;
+    private Date createdAt;
+    private Date completedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(Date completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
